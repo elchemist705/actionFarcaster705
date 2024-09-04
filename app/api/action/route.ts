58504581369dd74819941e1e6dc2a8835d2ef544
elchemist705 <bluejayultra@hotmail.com>
2024-09-04 15:48:17 -0400

    deleted app/api/frame/route.ts

--- app/api/action/route.ts
+++ app/api/action/route.ts
@@ -1,10 +1,27 @@
+import { FrameRequest, getFrameMessage } from '@coinbase/onchainkit/frame';
+import { NextRequest, NextResponse} from 'next/server';
+
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  const body: FrameRequest = await req.json();
-  const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAW_ONCHAIN_KIT' });
 
-  if (!isValid) {
-    return new NextResponse('Message not valid', { status: 500 });
-  }
-
-  return NextResponse.json({ message: 'Hello from the frame route' }), { status: 200 };
 }
+
+export async function POST(req: NextRequest): Promise<NextResponse> {
+    console.log('POST');
+    return getResponse(req): Promise<NextResponse>; {
+
+    }
+
+    export async function GET(req: NextRequest): Promise<NextResponse> {
+        console.log('GET');
+        return getResponse(req);
+    }
+
+    export const dynamic = 'force-dynamic';
+
+    const body: FrameRequest = await req.json();
+    const { isValid, message } = getFrameMessage(body, [ neynarApiKey: 'NEYNAR_ONCHAIN_KIT' ]);
+    if (!isValid) {
+        return new NextResponse('Message not valid', { status: 500 });
+    }
+
+    return NextResponse.json([ message: 'Hello from the frame route' ], { status: 200 });
